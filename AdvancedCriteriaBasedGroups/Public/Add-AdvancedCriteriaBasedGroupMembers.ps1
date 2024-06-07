ad4aa5de2477d9b361bf0fb68cf39cb9021a99db
Thomas Rogne Johansen <thomas.r.johansen@hotmail.com>
2024-06-07 15:38:06 +0200

    Chore: Add comments

--- AdvancedCriteriaBasedGroups/Public/Add-AdvancedCriteriaBasedGroupMembers.ps1
+++ AdvancedCriteriaBasedGroups/Public/Add-AdvancedCriteriaBasedGroupMembers.ps1
@@ -1,22 +1,55 @@
+<#
+.SYNOPSIS
+    Adds users to a group based on a specified criteria.
+
+.DESCRIPTION
+    This function adds users to a group based on a specified criteria. The criteria can be a script block that returns a boolean value for each user. If the script block returns $true, the user will be added to the group.
+    The function can also be used to add all members of a specified group to the group.
+
+.EXAMPLE
+    Add-AdvancedCriteriaBasedGroupMembers -Criteria { $_.UserPrincipalName -like "*@contoso.com" }
+
+    This example adds all users with a UserPrincipalName ending with @contoso.com to the group.
+
+.EXAMPLE
+    Add-AdvancedCriteriaBasedGroupMembers -Criteria { $_.JobTitle -like "Engineer" }
+
+    This example adds all users with a JobTitle equal to Engineer to the group.
+
+.EXAMPLE
+    Add-AdvancedCriteriaBasedGroupMembers -Criteria { ($_.EmployeeId -ge 1010) -and ($_.EmployeeId -le 1020) }
+
+    This example adds all users with an EmployeeId between 1010 and 1020 to the group.
+
+.EXAMPLE
+    Add-AdvancedCriteriaBasedGroupMembers -MembersOfGroupObjectId "2f4a2003-3684-4139-bff6-d737813991b5"
+
+    This example adds all members of the group with the object id 2f4a2003-3684-4139-bff6-d737813991b5 to the group.
+#>
 
 function Add-AdvancedCriteriaBasedGroupMembers {
     [CmdletBinding(DefaultParameterSetName = "Criteria")]
 
     Param(
+        #The criteria to use to determine which users to add to the group
         [Parameter(Mandatory = $true, ParameterSetName = "Criteria")]
         [ScriptBlock] $Criteria,
 
+        # The object id of the group where users should be fetched from
         [Parameter(Mandatory = $true, ParameterSetName = "GroupMembers")]
         [String] $MembersOfGroupObjectId,
 
+        # Switch to return the added users
         [Parameter(Mandatory = $false)]
         [Switch] $Passthru
     )
 
     Process {
+        # Call the Get-AdvancedCriteriaBasedGroupUsersFromCriteria function to filter out the users that should be added from cached users
         if ($PSCmdlet.ParameterSetName -eq "Criteria") {
             $UsersToAdd = Get-AdvancedCriteriaBasedGroupUsersFromCriteria -Criteria $Criteria
         }
+        # Get all members of the specified group if -MembersOfGroupObjectId is specified
         elseif ($PSCmdlet.ParameterSetName -eq "GroupMembers") {
             try {
                 Write-Verbose "Getting members of group $MembersOfGroupObjectId"
@@ -31,10 +64,12 @@ function Add-AdvancedCriteriaBasedGroupMembers {
             Write-Error "Unable to determine parameter set"
             return
         }
-
+        # Create statistics for user to add to group
         $count = 0
         $addedCount = 0
         $alreadyAddedCount = 0
+
+        # Loop through the users to add and ignore users that are already added
         $UsersToAdd |
         ForEach-Object {
             $count += 1
--- AdvancedCriteriaBasedGroups/Public/Start-AdvancedCriteriaBasedGroup.ps1
+++ AdvancedCriteriaBasedGroups/Public/Start-AdvancedCriteriaBasedGroup.ps1
@@ -1,8 +1,13 @@
 <#
+.SYNOPSIS
+    Starts working on a group to which criteria-based membership should be applied.
+
 .DESCRIPTION
+    This function starts working on a group to which criteria-based membership should be applied. It retrieves the existing members of the group and prepares to add and remove members based on criteria.
 
 .EXAMPLE
-Start-AdvancedCriteriaBasedGroup -ObjectId "404c71ff-bb33-4434-85e1-2e6c9863d33c" -Verbose
+    Start-AdvancedCriteriaBasedGroup -ObjectId "404c71ff-bb33-4434-85e1-2e6c9863d33c" -Verbose
+
 #>
 function Start-AdvancedCriteriaBasedGroup {
     [CmdletBinding()]
@@ -11,7 +16,7 @@ function Start-AdvancedCriteriaBasedGroup {
         # The object id of the group to work on
         [Parameter(Mandatory = $true)]
         [string] $ObjectId,
-
+        # Optional switch to use the beta endpoint
         [Parameter(Mandatory = $false)]
         [Switch] $UseGraphBetaEndpoint
     )
@@ -26,6 +31,7 @@ function Start-AdvancedCriteriaBasedGroup {
 
         Write-Verbose "Working on group $($Script:Group.DisplayName) ($($Script:Group.Id))"
 
+        # Creates a hashmap of all exising users in the specified group and gets all user properties from $Script:AllUsers
         $Script:ExistingMembers[$Script:Group.Id] = @{}
         Get-MgGroupMember -GroupId $Script:Group.Id -All |
         ForEach-Object {
@@ -34,6 +40,7 @@ function Start-AdvancedCriteriaBasedGroup {
 
         Write-Verbose "Group $($Script:Group.DisplayName) ($($Script:Group.Id)) has $($Script:ExistingMembers[$Script:Group.Id].Count) existing members"
 
+        # Initializes the hashmap of members to add
         $Script:AddedMembers[$Script:Group.Id] = @{}
     }
 }
